61f827086bf9278012676a468d635e92e878b492
Salvatore Tosti <saltosti@gmail.com>
2019-07-01 19:38:52 -0700

    Adds tile droping / spawning functionality

--- src/tetris_clj/core.clj
+++ src/tetris_clj/core.clj
@@ -1,4 +1,6 @@
 (ns tetris-clj.core
+    (:use
+        [tetris-clj.sprites :only [load-tile-map draw-tile]])
   (:require [quil.core :as q]
             [quil.middleware :as m])
   (:gen-class))
@@ -6,16 +8,158 @@
 (defn setup []
   (q/background 0)
   (q/frame-rate 30)
-  {:game {}})
+  {:game
+   {:last-tick (System/currentTimeMillis)
+    :size [32 32]
+    :tick-length 1000
+    :frozen []
+    :tile-map (load-tile-map
+                {:filename "resources/monochrome.png"
+                 :tile-size 16
+                 :columns 32
+                 :rows 32
+                 :column-spacing-size 1
+                 :row-spacing-size 1})}})
 
-(defn update [state]
-  state)
+(def tetrominoes
+  {:line {:tile-id :2 :offsets [[0 0] [1 0 ] [2 0] [3 0]]}
+   :l {:tile-id :2 :offsets [[0 0] [0 -1] [0 -2] [1 -2]]}
+   :s {:tile-id :2 :offsets [[0 0] [0 -1] [1 -1] [2 -1]]}
+   :z {:tile-id :2 :offsets [[0 0] [0 -1] [1 0] [1 1]]}
+   :square {:tile-id :2 :offsets [[0 0] [1 0] [0 -1] [1 -1]]}
+   :t {:tile-id :2 :offsets [[0 0] [1 0] [2 0] [1 -1]]}})
+
+(def directions
+  {:n [0 -1]
+   :s [0 1]
+   :w [-1 0]
+   :e [1 0]})
+
+(defn add-vectors [a b]
+  [(+ (first a) (first b))
+   (+ (second a) (second b))])
+
+(defn spawn-tetromino [state]
+  (if (nil? (get-in state [:game :active-tetromino]))
+    (assoc-in state [:game :active-tetromino]
+              {:position [20 20]
+               :tetromino (:l tetrominoes)})
+    state)) 
+
+(defn in?
+  "true if coll contains element"
+  [coll element]
+  (some #(= element %) coll))
+
+(defn- move-tetromino [state dir]
+   (let [tetromino (get-in state [:game :active-tetromino])
+         position (get-in tetromino [:position])
+         positions (map #(add-vectors position %) (:offsets (:tetromino tetromino)))
+         dir-vec (dir directions)
+         new-positions (map #(add-vectors dir-vec %) positions)
+         valid (every? true? (map #(inbounds? (get-in state [:game :size]) %) new-positions))
+         touch-frozen (some true? (map #(in? (get-in state [:game :frozen]) %) new-positions))]
+      (if (and valid (not touch-frozen))
+        (update-in state [:game :active-tetromino :position] #(add-vectors dir-vec %))
+        state)))
+
+(defn at-bottom? [state]
+  (let [tetromino (get-in state [:game :active-tetromino])
+        position (get-in tetromino [:position])
+        positions (map #(add-vectors position %) (:offsets (:tetromino tetromino)))
+        valid (every? true? (map #(inbounds? (get-in state [:game :size]) %) positions))
+        positions-at-bottom (map #(= (second %) (dec (second (get-in state [:game :size])))) positions)
+        any-at-bottom (not (every? false? positions-at-bottom))
+        above-frozen-positions (map #(add-vectors (:n directions) %) (get-in state [:game :frozen]))
+        touch-frozen (some #(in? above-frozen-positions %) positions)
+        ]
+    (println above-frozen-positions)
+    (or touch-frozen any-at-bottom)))
+
+(defn freeze-tetromino [state]
+   (let [tetromino (get-in state [:game :active-tetromino])
+         position (get-in tetromino [:position])
+         positions (map #(add-vectors position %) (:offsets (:tetromino tetromino)))
+         state (update-in state [:game :frozen] concat positions) ;;Need to conj here!
+         state (assoc-in state [:game :active-tetromino] nil)]
+     state))
+
+(defn drop-tetromino [state]
+  (let [state (move-tetromino state :s)]
+    (if (at-bottom? state)
+      (freeze-tetromino state)
+      state)))
+
+(defn do-tick [state]
+  (-> state
+      spawn-tetromino
+      drop-tetromino))
+
+(defn tick [state]
+   (let [current-time (System/currentTimeMillis)
+        tick-length (get-in state [:game :tick-length])
+        last-tick (get-in state [:game :last-tick])
+        next-tick (+ last-tick tick-length)]
+    (if (>= current-time next-tick)
+      (-> state
+          (do-tick)
+          (assoc-in [:game :last-tick] current-time))
+      state)))
+
+(defn quil-update [state]
+  (-> state
+      tick))
+
+(defn draw-tetromino [position tetromino tile-map]
+  (let [positions (map #(add-vectors position %) (:offsets tetromino))]
+    (doseq [[x y] positions]
+      (draw-tile x y tile-map (:tile-id tetromino) 16))))
+
+(defn draw-tetrominoes [tetrominoes tile-map]
+  (doseq [tetromino tetrominoes]
+    (when (not (nil? tetromino))
+      (draw-tetromino
+        (:position tetromino)
+        (:tetromino tetromino)
+        tile-map))))
+
+(defn draw-tiles [positions id tile-map]
+  (doseq [[x y] positions]
+    (draw-tile x y tile-map id 16)))
+
+(defn clear-screen [width height tile-map]
+  (doseq [x (range width)
+          y (range height)]
+    (draw-tile x y tile-map :0 16)))
 
 (defn draw [state]
-  state)
+  (clear-screen 32 32 (get-in state [:game :tile-map]))
+  (let [tetromino (get-in state [:game :active-tetromino])
+        frozen (get-in state [:game :frozen])]
+    (draw-tetrominoes [tetromino] (get-in state [:game :tile-map]))
+    (draw-tiles frozen :2 (get-in state [:game :tile-map]))))
+
+(defn inbounds? [[width height] [x y]]
+      (and
+        (>= x 0)
+        (< x width)
+        (>= y 0)
+        (< y height)))
+
+(defn move-left-tetromino [state]
+  (move-tetromino state :w))
+
+(defn move-right-tetromino [state]
+  (move-tetromino state :e))
 
 (defn process-input [state key-information]
-   (println (:key key-information)))
+  (if (nil? (get-in state [:game :active-tetromino]))
+    state
+    (case (:key key-information)
+      :a (move-left-tetromino state)
+      :d (move-right-tetromino state)
+      :s (drop-tetromino state)
+      state)))
 
 (defn -main []
   (q/defsketch game-sketch
@@ -23,7 +167,9 @@
     :size (let [[width height] [512 512]]
             [width height])
     :setup setup
-    :update update
+    :update quil-update
     :draw draw
     :key-pressed process-input
     :middleware [m/fun-mode]))
+
+(-main)
---
+++ src/tetris_clj/sprites.clj
@@ -0,0 +1,56 @@
+(ns tetris-clj.sprites
+  (:require [quil.core :as q]))
+
+(defn- get-offset [column-number spacing-size]
+  (* column-number spacing-size))
+
+(defn- get-start [number tile-size spacing-size]
+  (+ (* tile-size number) (get-offset number spacing-size)))
+
+(defn- get-tile [source-image
+                 column-number
+                 row-number
+                 {:keys [tile-size column-spacing-size row-spacing-size] :as args}]
+  (let [col-start (get-start column-number tile-size column-spacing-size)
+        row-start (get-start row-number tile-size row-spacing-size)
+        img (q/create-image tile-size tile-size :rgb)]
+    (q/copy source-image img [col-start row-start tile-size tile-size] [0 0 tile-size tile-size])
+    img))
+
+(defn- get-tile-row
+  ([image
+    row-number
+    {:keys [tile-size columns column-spacing-size row-spacing-size] :as args}
+    accumulator
+    counter]
+   (let [tile-id (-> row-number
+                    (* columns)
+                    (+ counter)
+                    (str)
+                    (keyword))
+        tile (get-tile image counter row-number args)
+        accumulator (assoc accumulator tile-id tile)]
+    (if (= counter (dec columns))
+      accumulator
+      (get-tile-row image row-number args accumulator (inc counter)))))
+  ([image
+    row-number
+    {:keys [tile-size columns column-spacing-size row-spacing-size] :as args}]
+   (get-tile-row image row-number args {} 0)))
+
+(defn load-tile-map [{:keys [filename tile-size columns rows column-spacing-size row-spacing-size] :as args}]
+   (let [source-image (q/load-image filename)]
+     (while (not (q/loaded? source-image))
+       nil)
+     (->> (range rows)
+          (map #(get-tile-row source-image % args))
+          (into {}))))
+
+(defn- draw-image [x y img tile-size]
+  (when (q/loaded? img)
+    (q/image img (* x tile-size) (* y tile-size))))
+
+(defn draw-tile [x y tile-map id tile-size]
+  (let [img (id tile-map)]
+    (when (q/loaded? img)
+      (q/image img (* x tile-size) (* y tile-size)))))
